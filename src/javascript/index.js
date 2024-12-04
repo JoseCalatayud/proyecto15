cef33066f73e1d3eaaeeb931fedf59047c03f18e
curso05 <notiene@notiene.es>
2024-12-04 14:02:43 +0100

    kkk

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,1 +1,11 @@
+import $ from './jquery'
 console.log('Hola mundo... por segunda vez');
+console.log('hola de nuevo') 
+console.log('ojo que estamos al loro')
+// document.getElementById('boton').onclick = function () {
+//     alert ('Cuidado')
+// }
+$("#boton").click(function(){
+    alert('Hola')
+})
+
